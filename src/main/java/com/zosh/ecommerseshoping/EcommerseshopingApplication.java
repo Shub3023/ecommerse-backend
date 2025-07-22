3b8095e8953adfc8a1e6c684e6b32114772f2631
Shub3023 <shubhamkale50752@gmail.com>
2025-07-23 01:13:16 +0530

    spring security configuration

---
+++ src/main/java/com/zosh/ecommerseshoping/EcommerseshopingApplication.java
@@ -0,0 +1,13 @@
+package com.zosh.ecommerseshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerseshopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerseshopingApplication.class, args);
+	}
+
+}
